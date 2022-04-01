b766a765df1fcac8ee6a15aab33164374e41f655
username917 <86450720+username917@users.noreply.github.com>
2022-03-31 23:36:40 -0400

    Add files via upload

---
+++ demospring-boot-rest-example/src/main/java/com/javatpoint/DemospringBootRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringBootRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringBootRestExampleApplication.class, args);
+	}
+
+}
